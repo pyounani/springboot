bd37e9c2a75322bb71c10a714e1ea884bae39c37
pyounani <yndbsk9372@gmail.com>
2024-11-10 13:00:32 +0900

    [Feat] 자바 시스템 속성 확인하기

--- external/src/test/java/hello/external/OsEnv.java
+++ external/src/test/java/hello/external/OsEnv.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Map;
 
 @Slf4j
-public class OsEnc {
+public class OsEnv {
 
     public static void main(String[] args) {
         Map<String, String> envMap = System.getenv();
