2527f286b8c5ce760e4b2a3b4c6da83595a94940
pyounani <yndbsk9372@gmail.com>
2024-11-11 11:18:25 +0900

    [Feat] @Profile 적용

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -11,8 +11,8 @@ import org.springframework.context.annotation.Import;
 //@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceConfigV2.class)
 @Import(MyDataSourceConfigV3.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")
-@ConfigurationPropertiesScan
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
+//@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
